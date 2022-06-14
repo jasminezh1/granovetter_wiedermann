01309d7486d9afbf785e2290c88583454491da5d
Jasmine Zhang <jasminez@princeton.edu>
2022-06-14 11:31:18 +0200

    incorrect list version

--- distribution.py
+++ distribution.py
@@ -5,29 +5,33 @@ import matplotlib.pyplot as plt
 # attempt to implement the threshold distribution
 rho = 0.5       # threshold fraction
 k = 10      # average degree
-r = 100     # overll share of active nodes
 
 # solve F(r) = (r-a)/c
 
+# r is overall share of active nodes
 def f(r):
-    size_of_r = len(r)
+    print("beginning of function")
+    print("this is r: ",r)
+    size_of_r = 1
+    if(type(r)==list):
+        size_of_r = len(r)
     total_summation = [0]*size_of_r
-    print("should be 10 zeros: ", total_summation)
+    print("should be 10 zeros sometimes: ", total_summation)
     for b in range(1, 20):
-        #print("yikes")
         inner_max = rho * b / (1 - rho)
         inner_max = int(inner_max)
         inner_sum = 0
+
         for a in range(1, inner_max):
-            #print("extra yikes")
             inner_sum += ((k*r)**a)/math.factorial(a)
+       
         outer_sum = ((k-k*r)**b)/math.factorial(b)
-        #print("outer", outer_sum)
-        #print("inner", inner_sum)
         temp = np.multiply(outer_sum, inner_sum)
-        print("temp: ",temp)
+        #print("temp: ",temp)
         total_summation += temp
     return 1 - math.exp(-k) * total_summation
+
+# print("idk: ", f(0.1)) # this is fine
 
 x = np.linspace(0,1,10)
 #print(x)
